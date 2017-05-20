f3a8db8b61fa0118fe6ee994b1450bbf0b349d81
chersquwn <chersquwn@qq.com>
2017-05-20 19:09:21 +0800

    add Switch component

---
+++ example/views/Switch.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <header>
+      Switch
+    </header>
+    <div class="group">
+      <mo-switch v-model="value">
+        {{ value }}
+      </mo-switch>
+    </div>
+    <div class="group">
+      <mo-switch v-model="value2" disabled>
+        禁用
+      </mo-switch>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: false,
+      value2: true
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+header {
+  text-align: center;
+}
+.group {
+  margin: 10px 0;
+  vertical-align: middle;
+}
+</style>
+
---
+++ src/components/switch/switch.vue
@@ -0,0 +1,38 @@
+<template>
+  <label class="vumo-switch">
+    <input 
+      type="checkbox" 
+      class="vumo-switch-input"
+      :disabled="disabled"
+      v-model="currentValue">
+    <slot></slot>
+  </label>
+</template>
+
+<script>
+export default {
+  name: 'mo-switch',
+  props: {
+    disabled: Boolean,
+    value: {
+      type: Boolean,
+      default: false
+    },
+  },
+  data() {
+    return {
+      currentValue: this.value
+    }
+  },
+  watch: {
+    currentValue(val) {
+      this.$emit('input', val)
+    }
+  }
+}
+</script>
+
+
+<style lang="scss">
+@import "./switch.scss";
+</style>
