af05e474712644ea6a437c3b2f9361eb732be29a
chersquwn <815533240@qq.com>
2017-06-02 13:52:49 +0800

    add Card component

---
+++ src/components/card/card.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="vumo-card">
+    <header class="vumo-card-header">
+      <slot name="header"></slot>
+    </header>
+    <div class="vumo-card-content">
+      <slot></slot>
+    </div>
+    <div class="vumo-card-footer">
+      <slot name="vumo-card-footer"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'mo-card'
+}
+</script>
+
+<style lang="scss">
+@import "./card.scss";
+</style>
